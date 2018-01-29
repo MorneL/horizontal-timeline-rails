dec5f04e778aedffdaf23be78462539c55fe3a60
Morne lotze <morne.lotze@getsmarter.com>
2018-01-29 19:41:14 +0200

    removed primary stylesheet

--- lib/horizontal-timeline-rails/version.rb
+++ lib/horizontal-timeline-rails/version.rb
@@ -1,8 +1,8 @@
 module Horizontal
   module Timeline
     module Rails
-      VERSION = '0.0.2'
-      HORIZONTAL_TIMELINE_VERSION = '0.0.2'
+      VERSION = '0.0.3'
+      HORIZONTAL_TIMELINE_VERSION = '0.0.3'
     end
   end
 end
