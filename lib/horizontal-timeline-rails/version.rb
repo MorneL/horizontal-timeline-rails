b3b939f5bf5226202a8e7fb9ed58ca033e400277
Morne lotze <morne.lotze@getsmarter.com>
2018-01-29 16:05:09 +0200

    version 0.0.2

--- lib/horizontal-timeline-rails/version.rb
+++ lib/horizontal-timeline-rails/version.rb
@@ -1,8 +1,8 @@
 module Horizontal
   module Timeline
     module Rails
-      VERSION = '0.0.1'
-      HORIZONTAL_TIMELINE_VERSION = '0.0.1'
+      VERSION = '0.0.2'
+      HORIZONTAL_TIMELINE_VERSION = '0.0.2'
     end
   end
 end
